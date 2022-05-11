935ce34a53c994c4ccd63592ded6a84676ebd55d
petifox <73014276+petifox@users.noreply.github.com>
2022-05-11 22:18:11 +0200

    idk

--- terrain_generation/Assets/Scripts/TerrainFace.cs
+++ terrain_generation/Assets/Scripts/TerrainFace.cs
@@ -1,17 +1,19 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TerrainFace : MonoBehaviour
-{
+public class TerrainFace {
+
+    ShapeGenerator shapeGenerator;
     Mesh mesh;
     int resolution;
     Vector3 localUp;
     Vector3 axisA;
     Vector3 axisB;
 
-    public TerrainFace(Mesh mesh, int resolution, Vector3 localUp)
+    public TerrainFace(ShapeGenerator shapeGenerator, Mesh mesh, int resolution, Vector3 localUp)
     {
+        this.shapeGenerator = shapeGenerator;
         this.mesh = mesh;
         this.resolution = resolution;
         this.localUp = localUp;
@@ -24,8 +26,6 @@ public class TerrainFace : MonoBehaviour
     {
         Vector3[] vertices = new Vector3[resolution * resolution];
         int[] triangles = new int[(resolution - 1) * (resolution - 1) * 6];
-        Vector3[] normals = new Vector3[resolution * resolution];
-
         int triIndex = 0;
 
         for (int y = 0; y < resolution; y++)
@@ -34,31 +34,26 @@ public class TerrainFace : MonoBehaviour
             {
                 int i = x + y * resolution;
                 Vector2 percent = new Vector2(x, y) / (resolution - 1);
-                Vector3 pointOnUnitCube = localUp + (percent.x - 0.5f) * 2 * axisA + (percent.y - 0.5f) * 2 * axisB;
-                Vector3 PointOnUnitSphere = pointOnUnitCube.normalized;
-                vertices[i] = PointOnUnitSphere;
+                Vector3 pointOnUnitCube = localUp + (percent.x - .5f) * 2 * axisA + (percent.y - .5f) * 2 * axisB;
+                Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+                vertices[i] = shapeGenerator.CalculatePointOnPlanet(pointOnUnitSphere);
 
                 if (x != resolution - 1 && y != resolution - 1)
                 {
-                    triangles[triIndex + 0] = i;
+                    triangles[triIndex] = i;
                     triangles[triIndex + 1] = i + resolution + 1;
                     triangles[triIndex + 2] = i + resolution;
 
                     triangles[triIndex + 3] = i;
                     triangles[triIndex + 4] = i + 1;
                     triangles[triIndex + 5] = i + resolution + 1;
-
                     triIndex += 6;
                 }
-
-                normals[i] = vertices[i].normalized;
             }
         }
-
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        mesh.normals = normals;
-        //mesh.RecalculateNormals();
+        mesh.RecalculateNormals();
     }
 }
