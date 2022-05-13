01fae8dd250d301c48551877eb32d5314daad353
petifox <73014276+petifox@users.noreply.github.com>
2022-05-13 08:18:15 +0200

    Complex noise

--- terrain_generation/Assets/Scripts/Planet.cs
+++ terrain_generation/Assets/Scripts/Planet.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 
 public class Planet : MonoBehaviour {
 
-    public Material material;
-
     [Range(2,256)]
     public int resolution = 10;
     public bool autoUpdate = true;
+    public enum FaceRenderMask { All, Top, Bottom, Left, Right, Front, Back };
+    public FaceRenderMask faceRenderMask;
 
     public ShapeSettings shapeSettings;
     public ColourSettings colourSettings;
+
+    public Material material;
 
     [HideInInspector]
     public bool shapeSettingsFoldout;
@@ -50,6 +52,8 @@ public class Planet : MonoBehaviour {
             }
 
             terrainFaces[i] = new TerrainFace(shapeGenerator, meshFilters[i].sharedMesh, resolution, directions[i]);
+            bool renderFace = faceRenderMask == FaceRenderMask.All || (int)faceRenderMask - 1 == i;
+            meshFilters[i].gameObject.SetActive(renderFace);
         }
     }
 
@@ -80,9 +84,12 @@ public class Planet : MonoBehaviour {
 
     void GenerateMesh()
     {
-        foreach (TerrainFace face in terrainFaces)
+        for (int i = 0; i < 6; i++)
         {
-            face.ConstructMesh();
+            if (meshFilters[i].gameObject.activeSelf)
+            {
+                terrainFaces[i].ConstructMesh();
+            }
         }
     }
 
